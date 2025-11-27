95e7be41508a419fafadfff85d7d499ab4c49d2f
HansJack <2310210201@tiangong.edu.cn>
2025-11-27 14:54:57 +0800

    build(Plugin): 更新版本号至 pro_0.1.5

--- Plugin.php
+++ Plugin.php
@@ -8,7 +8,7 @@ require_once __TYPECHO_ROOT_DIR__ . '/usr/plugins/MediaLibrary/includes/WebDAVPr
  * 
  * @package MediaLibrary
  * @author HansJack
- * @version pro_0.1.4
+ * @version pro_0.1.5
  * @link http://www.hansjack.com/
  */
 class MediaLibrary_Plugin implements Typecho_Plugin_Interface
